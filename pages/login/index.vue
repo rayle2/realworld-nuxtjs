d743e30ff6b628c59dd929a05ae96c7fad7b33d0
houlei <houlei@changxindata.com>
2020-08-20 12:11:13 +0800

    注册页权限

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -79,7 +79,7 @@ const Cookie = process.client ? require('js-cookie') : undefined
 
 export default {
   name: 'LoginIndex',
-  // middleware: ['notAuthenticated'],
+  middleware: ['notAuthenticated'],
   data () {
     return {
       user: {
